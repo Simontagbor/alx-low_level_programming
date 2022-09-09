abd42722e8dca14deb5fc5b258e894e6361d80c6
Simontagbor <simontagbor360@gmail.com>
2022-09-09 15:18:31 -0700

    add c program to print the size of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a c code to print size of types
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an char: %lu bytes(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
