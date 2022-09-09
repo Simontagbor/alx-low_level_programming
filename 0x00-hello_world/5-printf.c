dbe37f2c91bb8cae8a36d2795ff3f81e44ab50c6
Simontagbor <simontagbor360@gmail.com>
2022-09-09 14:54:20 -0700

    fix betty warning

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,19 +7,20 @@
 #include <string.h>
 
 /**
- * Main  - Entry Point
+ * main  - Entry Point
  *
  * Description : a simple code to print  string to stdout
  *
- * Return value: 0 Sucess
-
+ * Return: Always 0 ( Success)
  */
 
 int main(void)
 {
+	/*use the printf function*/
 	printf("with proper grammar, but the outcome is a piece of art,\r\n");
 
 	return (0);
+
 }
 
 
