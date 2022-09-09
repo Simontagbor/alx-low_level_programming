a78a9d87a068101aa019d2777b04168b67cc154d
Simontagbor <simontagbor360@gmail.com>
2022-09-09 14:39:36 -0700

    add new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -17,7 +17,7 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\r\n");
 
 	return (0);
 }
