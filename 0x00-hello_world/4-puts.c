88716269134b3c3fb7fa42ce996d7094bafc98c3
Simontagbor <simontagbor360@gmail.com>
2022-09-09 13:38:29 -0700

    add my first c program in alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * A c program to learn to use puts function
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	/*use the function*/
+	puts("Programming is like building a multilingual puzzle");
+
+	return 0;
+
+
+}
