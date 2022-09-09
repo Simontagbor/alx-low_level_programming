3a9d29071f5fb908a9b8338a2e544211b152d75c
Simontagbor <simontagbor360@gmail.com>
2022-09-09 13:54:54 -0700

    add my first c program in alx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main(void)
+{
 	/*use the function*/
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 
 
 }
