2d59cb27b059215e16ff0fef5cc11103ec40badc
Simontagbor <simontagbor360@gmail.com>
2022-09-09 14:31:39 -0700

    add new c file to print message using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+/*
+ * A c program to practice with printf
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Main  - Entry Point
+ *
+ * Description : a simple code to print  string to stdout
+ *
+ * Return value: 0 Sucess
+
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
+
+
