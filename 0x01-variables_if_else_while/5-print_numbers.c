f2baf94edf643ee85b64db6c3819cfa5a70ffc83
Simontagbor <simontagbor360@gmail.com>
2022-09-11 04:40:12 -0700

    add source file to print numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  a function to print base numers up to 10
+ *
+ * Description: usimmg the main function to print numbers from 0 to 10
+ * Return: 0
+ */
+int main(void)
+{
+
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+	return (0);
+}
+
