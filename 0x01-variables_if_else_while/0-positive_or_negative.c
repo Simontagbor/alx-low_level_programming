268c138b800c93ed421b47a0a983ea1d9c3ebe81
Simontagbor <simontagbor360@gmail.com>
2022-09-10 17:05:23 -0700

    add script to print random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,14 +22,14 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		puts("is positive");
+		printf("%d is positive", n);
 	}
 	else if (n == 0)
 	{
-		puts("is zero");
+		printf("%d is zero", n);
 	}
 	else
-		puts("is negative");
+		printf("%d is negative", n);
 
 	return (0);
 }
