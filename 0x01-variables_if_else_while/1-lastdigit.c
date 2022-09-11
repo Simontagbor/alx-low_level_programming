eccc610b42296889ebf2c0573113167e2bfa752c
Simontagbor <simontagbor360@gmail.com>
2022-09-11 02:27:17 -0700

    add source and executable file for random number program

---
+++ 0x01-variables_if_else_while/1-lastdigit.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a file to generate random number
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+
+{
+
+	int n, lastDigit;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	lastDigit = n % 10;
+
+	/* your code goes there */
+
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else if (lastDigit < 6 && lastDigit > 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 not 0\n", n, lastDigit);
+	}
+	return (0);
+
+
+}
