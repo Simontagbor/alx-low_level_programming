1fbd66052e96609447441f0a6ee2cc94a7eac283
Simontagbor <simontagbor360@gmail.com>
2022-09-12 10:03:22 -0700

    add script to print combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - a prmain function to print combined char and  basic int
+ *
+ * Description: this creates a character combinations of  char and int
+ * Return: 0
+ */
+
+
+
+int main(void)
+
+{
+
+
+	char c;
+	char d;
+
+	for (c = '0'; c <= '8'; c++)
+
+	{
+
+
+		for (d = '0'; d <= '9'; d++)
+		{
+			if (d != c && c < d)
+			{
+				putchar(c);
+				putchar(d);
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
