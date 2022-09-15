164fbc5fe17293dc35f3c79b9dc9452586ff7fa3
Simontagbor <simontagbor360@gmail.com>
2022-09-15 16:25:23 -0700

    addd sript to test positive_or_negative() function

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,20 +15,18 @@
 int main(void)
 
 {
-
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10)
+/*	while (i < 10)
 	{
 		putchar(i);
-		1++;
-	}
 
+	}
+*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
