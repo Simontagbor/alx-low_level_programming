25817f298a8979647cbc8821a7279400adffd2f6
Simontagbor <simontagbor360@gmail.com>
2022-09-28 09:16:13 -0700

    add script to concateneate strings in c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _strcat - a function to concatenate string constants
+ * @*dest: an argument of the char type that references a first string
+ * @*src: an argument of the char type that references a second string
+ *
+ * Return: a pointer to resulting sring dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] =  src[b];
+		b++;
+		a++;
+	}
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Simon Tagbor
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
