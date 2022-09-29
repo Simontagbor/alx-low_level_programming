9a2cffaa0c6deb3da46a014d370ecc82bce61fc1
Simontagbor <simontagbor360@gmail.com>
2022-09-29 16:45:03 -0700

    add scripts to test factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,8 @@
-/**
+/*
  * file - 3-factorial
  * Author: Simon Tagbor
  */
+
 #include "main.h"
 
 /**
@@ -14,13 +15,13 @@ int factorial(int n)
 {
 	int result = n;
 
-	if ( n <= 1)
+	if (n == 0)
 		return (1);
-	if ( n < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	result = n * factorial(n-1);
+	result *= factorial(n - 1);
 	return (result);
 
 }
