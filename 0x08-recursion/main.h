0ebbc0dc9703838d4e80645c2d7f3b27e6992eeb
Simontagbor <simontagbor360@gmail.com>
2022-09-29 15:02:42 -0700

    add files to new directory for practicing recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Simon Tagbor
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x08-recursion directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
