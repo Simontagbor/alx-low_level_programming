e4910c1abd6ab4bfea8e3cd2d52a5bed9d3c17d5
Simontagbor <simontagbor360@gmail.com>
2022-09-29 17:20:22 -0700

    add scripts to test raising to power by recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,13 +16,15 @@
 int _pow_recursion(int x, int y)
 {
 	int power = x;
+
 	if (y < 0)
 	{
 		return (-1);
 	}
+
 	else if (y == 0)
 		return (1);
 
-	power *=_pow_recursion(x, y - 1);
+	power *= _pow_recursion(x, y - 1);
 	return (power);
 }
