cb7cd6c1722a508137b80b0f8b2f392587856d11
Simontagbor <simontagbor360@gmail.com>
2022-10-05 16:44:46 -0700

    hot fix

---
+++ 0x0B-malloc_free.c/template.c
@@ -0,0 +1,22 @@
+
+/* file - description
+ * Author: Simon Tagbor @Simontagbor
+ */
+
+#include "main.h"
+
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return:
+ */
+
+int _isupper(int c)
+
+{
+
+
+}
+
