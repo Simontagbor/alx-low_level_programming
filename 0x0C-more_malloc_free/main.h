ecc1dc367a802ce6f0c5043b5c80804383c2a814
Simontagbor <simontagbor360@gmail.com>
2022-10-06 22:05:50 -0700

    add script to allocate memory

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Simon Tagbor
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x0C-malloc_free  directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
