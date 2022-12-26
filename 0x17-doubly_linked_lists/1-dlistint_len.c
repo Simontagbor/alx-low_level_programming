93f4491af535a0a8882c8dbf6849169cb8c33138
Simontagbor <simontagbor360@gmail.com>
2022-12-26 08:50:32 -0800

    add script to count elements of a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+/**
+ * file: 1-dlistint_len.c
+ * Auth: Simon Tagbor <simontagbor360@gmail.com>
+ */
+
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of elements in a linked list.
+ */
+
+size_t dlistint_len(const dlistint_t *h) {
+    size_t len = 0;
+
+    /*loop through elements*/
+    while(h){
+        len++;
+        h = h->next;
+    }
+    return (len);
+}
