427b7d72dbfb34984540a94169c8f9569094dcd2
Simontagbor <simontagbor360@gmail.com>
2022-12-26 12:23:01 -0800

    add function to free memory allocated for nodes

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+/*
+ * File: 4-free_dlistint.c
+ * Auth: Simon Tagbor
+ */
+
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a linked dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
