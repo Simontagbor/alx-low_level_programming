7b3a2a5ed766b1955db96cd6d6d0dd1420a1996c
Simontagbor <simontagbor360@gmail.com>
2023-04-13 12:48:07 -0700

    fix include guard

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,8 @@
 #ifndef  SEARCH_ALGOS_H_
-#define SEARRCH_ALGOS_H_
+#define  SEARCH_ALGOS_H_
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-#endif
+
+#endif /* SEARCH_ALGOS_H */
