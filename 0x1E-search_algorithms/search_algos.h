a2b968a238770b0d65fede82d5e79a065ece6bb3
Simontagbor <simontagbor360@gmail.com>
2023-07-12 00:47:01 -0700

    add header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef LINEAR_SEARCH_H
+#define LINEAR_SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* LINEAR_SEARCH_H */
+
